fd6e029d21e856c61b158d703266f257f8961ce4
Ante Dragoje <antekrtalic2@gmail.com>
2023-02-23 21:50:49 +0100

    Create js file for backend and add dockerfile

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -4,7 +4,11 @@ const app = express();
 app.use(express.json());
 
 app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+    console.log("Server is running on port 3001 and is updated");
+});
+
+app.get("/", (req, res) => {
+    res.send("Get request returned message!");
 });
 
 module.exports = app;
