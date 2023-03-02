137f083479466285da0ccf02edf32bf1a44697f7
Ante Dragoje <antekrtalic2@gmail.com>
2023-03-02 21:43:40 +0100

    Edit route in app js file

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
 app.use(express.json());
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'winpeak'
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database");
+})
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001 and is updated");
