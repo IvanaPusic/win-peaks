c4b57e1397c7d2d4d01fde38d7988c7516590ea6
Ante Dragoje <antekrtalic2@gmail.com>
2023-03-12 08:18:35 +0100

    Last changes on docker-compose file express-server

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -2,25 +2,21 @@
 import express from "express";
 // const express = require("express");
 const app = express();
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
 app.use(express.json());
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'user',
-    password: 'password',
-    database: 'winpeak'
-});
+// const conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'user',
+//     password: 'password',
+//     database: 'winpeak'
+// });
 
-conn.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to the database");
-})
-
-app.listen(3001, () => {
-    console.log("Server is running on port 3001 and is updated");
-});
+// conn.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected to the database");
+// })
 
 app.get("/", (req, res) => {
     res.send("Get request returned message!");
@@ -29,6 +25,13 @@ app.get("/", (req, res) => {
 const addNewDriver = (req, res) => {
     res.status(StatusCode.OK).json("Hi from the driver");
 }
-// module.exports = app;
+// module.exports = app; 
+
+
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001 and is updated");
+});
+
 
 export { addNewDriver };
